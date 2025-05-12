ff31626a7a76bd55aea2744f75a443b6cf1e4822
mrnykterstein <mrnykterstein@yandex-staff.ru>
2025-05-12 16:13:41 +0300

    kafka

---
+++ scripts/kafka/install_kafka.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if (($OS == 'ubuntu')) ; then
+  sudo apt install -y default-jre
+else
+  sudo brew install openjdk
+fi
+
+curl https://dlcdn.apache.org/kafka/3.8.0/kafka_2.13-3.8.0.tgz -o kafka.tgz
+mkdir -p /etc/kafka
+tar xf kafka.tgz --directory=$kafka_path
+cp -r /etc/kafka/kafka_2.13-3.8.0/* $kafka_path
+rm -rf /etc/kafka/kafka_2.13-3.8.0
+rm kafka.tgz
