c6bf9a65a0d55c6bf7f2b7f5977bf56d10afbd7a
mrnykterstein <mrnykterstein@yandex-staff.ru>
2025-05-12 20:47:39 +0300

    kafka

--- scripts/kafka/install_kafka.sh
+++ scripts/kafka/install_kafka.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-if (($OS == 'ubuntu')) ; then
+if [ "$(uname)" = "Darwin" ]; then
   sudo apt install -y default-jre
 else
   sudo brew install openjdk
 fi
 
+if ! [ -v "$(KAFKA_PATH)" ]; then
+  if [ "$(uname)" = "Darwin" ]; then
+    export KAFKA_PATH="/opt/homebrew/opt/kafka/libexec"
+  else
+    export KAFKA_PATH="/etc/kafka"
+  fi
+fi
+
 curl https://dlcdn.apache.org/kafka/3.8.0/kafka_2.13-3.8.0.tgz -o kafka.tgz
-mkdir -p $kafka_path
-tar xf kafka.tgz --directory=$kafka_path
-cp -r /etc/kafka/kafka_2.13-3.8.0/* $kafka_path
+mkdir -p $KAFKA_PATH
+tar xf kafka.tgz --directory=$KAFKA_PATH
+cp -r /etc/kafka/kafka_2.13-3.8.0/* $KAFKA_PATH
 rm -rf /etc/kafka/kafka_2.13-3.8.0
 rm kafka.tgz
