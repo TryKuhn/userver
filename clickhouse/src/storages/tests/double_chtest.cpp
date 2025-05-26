cf2fa952f58041f491b4d194450df7f9eece67a7
mrnykterstein <mrnykterstein@yandex-staff.ru>
2025-05-26 12:50:09 +0300

    debug test

--- clickhouse/src/storages/tests/double_chtest.cpp
+++ clickhouse/src/storages/tests/double_chtest.cpp
@@ -43,7 +43,7 @@ UTEST(Double, InsertSelectWhere) {
 
     const auto select_data =
         cluster->Execute(query, storages::clickhouse::io::FloatingWithPrecision<double, 2>(0.5)).As<DataWithDoubles>();
-    throw std::runtime_error("Debug");
+    assert(select_data.doubles.size() == 3);
     ASSERT_EQ(select_data.doubles.size(), 3);
 
     for (std::size_t i = 0; i < 2; ++i) {
